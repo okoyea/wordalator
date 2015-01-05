0077a2f73e615544c6fa084b57665c3f44f53016
O'Koyea Huff-Boone <okoyeahb@gmail.com>
2015-01-05 00:05:45 -0600

    added custom setters

--- lib/wordalator/wdlt.rb
+++ lib/wordalator/wdlt.rb
@@ -11,29 +11,24 @@ module Wordalator
     validates_presence_of :query
 
     attr_accessor :queries
+    attr_reader :nums, :ops
 
     def initialize(queries)
       @queries = queries
     end
 
-    def do_the_math(nums, ops)
-
-      #validations
-      #nums cant be less than one integer
-      #ops must always be one less that the count of nums
-
+    def calculate
       #to ensure we can calculate decimal numbers
-      nums.map!(&:to_f)
+      @nums.map!(&:to_f)
 
       #initial result
-      result = nums[0].send(MATCHERS[ops[0]],nums[1])
+      result = @nums[0].send(MATCHERS[@ops[0]],@nums[1])
 
       #for multiple operations in one sentence
       #uses the initial result to do any extra calculations
-      if ops.count > 1
-        ops[1..-1].each_with_index do |op, i|
-          j = i+2
-          result = result.send(MATCHERS[op],nums[j])
+      if @ops.count > 1
+        @ops[1..-1].each_with_index do |op, i|
+          result = result.send(MATCHERS[op],@nums[i+2])
         end
       end
 
@@ -43,17 +38,39 @@ module Wordalator
     def parse
       results = []
 
-      queries.each do |q|
-        nums,ops = WDLTQueryProcessor.new(q).process
+      @queries.each do |q|
+        #call custom setter methods
+        self.nums, self.ops = WDLTQueryProcessor.new(q).process
 
-        if (queries.count > 1)
-          results << do_the_math(nums, ops)
+        # return an array only when there are multiple queries
+        if (@queries.count > 1)
+          results << calculate
         else
-          results = do_the_math(nums, ops)
+          results = calculate
         end
       end
 
       results
+    end
+
+    private
+
+    def nums=(nums)
+      @nums_count = nums.count
+
+      if nums == nil || nums.count <= 1
+        raise ArgumentError.new('A query must contain at least two numbers')
+      end
+
+      @nums = nums
+    end
+
+    def ops=(ops)
+      if ops == nil || ops.count != @nums_count - 1
+        raise ArgumentError.new('Too many operators exist in your query')
+      end
+
+      @ops = ops
     end
   end
 end
